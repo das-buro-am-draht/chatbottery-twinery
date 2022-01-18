2d95f5a65d235c5cc998405438426b43579d41d5
Thorsten Breug <thorsten.breug@ext.conrad.com>
2022-01-18 16:15:51 +0100

    format version

--- src/data/format-versions.js
+++ src/data/format-versions.js
@@ -45,8 +45,10 @@ module.exports = {
 		const filtered = formats.filter(format => 
 			format.name === name && 
 			semverUtils.parse(format.version).major === semverUtils.parse(version).major);
-		if (filtered.length === 0) {
+		if (!filtered.length) {
 			return null;
+		} else if (filtered.length === 1) {
+			return filtered[0];
 		} else {
 			return filtered.reduce((prev, current) => {
 				const pVer = semverUtils.parse(prev.version);
