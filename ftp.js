060de98f264c6f3421eca067d6a476201c187b0e
Cezary Śliwiński <cezary.sliwinski@pidoco.com>
2022-05-06 10:23:18 +0200

    conf(ftp): ftp script

---
+++ ftp.js
@@ -0,0 +1,35 @@
+const Deploy = require("ftp-deploy");
+
+const ftpDeploy = new Deploy();
+
+const config = {
+  host: process.env.FTP_HOST,
+  user: process.env.FTP_USER_NAME,
+  password: process.env.FTP_PASSWORD,
+  port: 21,
+  localRoot: `${__dirname}/dist/web`,
+  remoteRoot: `${process.env.FTP_REMOTE_FOLDER}/${process.env.CONTEXT}/${process.env.HEAD}`,
+  include: ["*"],
+  deleteRemote: false,
+};
+ftpDeploy.deploy(config, function (err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("finished:", res);
+  }
+});
+ftpDeploy.on("uploading", function (data) {
+  data.totalFilesCount;
+  data.transferredFileCount;
+  data.filename;
+});
+ftpDeploy.on("uploaded", function (data) {
+  console.log(data);
+});
+ftpDeploy.on("log", function (data) {
+  console.log(data);
+});
+ftpDeploy.on("upload-error", function (data) {
+  console.log(data.err);
+});
