0275d2581f911ce40d6a43da3b22aea3855a0029
Thorsten Breug <thorsten.breug@ext.conrad.com>
2022-06-23 17:05:57 +0200

    tracking settings

--- src/utils/isTrackingScriptActivated.js
+++
@@ -1,7 +0,0 @@
-const isTrackingScriptActivated = (tracking = {}) => {
-	const { matomo = false, google = false } = tracking || {};
-
-	return matomo || google;
-};
-
-module.exports = isTrackingScriptActivated;
