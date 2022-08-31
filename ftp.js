fbae2f2fdb5f50b94844b888a465986dd3685855
Thorsten Breug <thorsten.breug@ext.conrad.com>
2022-08-31 10:03:09 +0200

    use sftp

--- ftp.js
+++ ftp.js
@@ -6,19 +6,13 @@ const config = {
   host: process.env.FTP_HOST,
   user: process.env.FTP_USER_NAME,
   password: process.env.FTP_PASSWORD,
-  port: 21,
+  port: 22,
   localRoot: `${__dirname}/dist/web`,
   remoteRoot: `${process.env.FTP_REMOTE_FOLDER}/${process.env.CONTEXT}/${process.env.HEAD}`,
   include: ["*"],
   deleteRemote: false,
+  sftp: true,
 };
-ftpDeploy.deploy(config, function (err, res) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("finished:", res);
-  }
-});
 ftpDeploy.on("uploading", function (data) {
   data.totalFilesCount;
   data.transferredFileCount;
@@ -32,4 +26,11 @@ ftpDeploy.on("log", function (data) {
 });
 ftpDeploy.on("upload-error", function (data) {
   console.log(data.err);
+});
+ftpDeploy.deploy(config, function (err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("finished:", res);
+  }
 });
