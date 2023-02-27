ed152fd1a26a64d80847964a4cb18c4d76d65387
hbogadtke <heiner.bogadtke@pidoco.com>
2023-02-27 16:46:39 +0100

    721 many tags are truncated in edit view

--- src/editors/passage/tag-editor/index.js
+++ src/editors/passage/tag-editor/index.js
@@ -1,13 +1,13 @@
 /* An editor for adding and removing tags from a passage. */
 
 const Vue = require('vue');
-const { setTagColorInStory } = require('../../../data/actions/story');
-const { updatePassage } = require('../../../data/actions/passage');
+const {setTagColorInStory} = require('../../../data/actions/story');
+const {updatePassage} = require('../../../data/actions/passage');
 const TagsDialog = require('./tag-dialog');
-const { openai } = require('../../../common/app/openai');
+const {openai} = require('../../../common/app/openai');
 const notify = require('../../../ui/notify');
 const uniq = require('lodash.uniq');
-const { buzzwordFromTag } = require('../../../utils/common');
+const {buzzwordFromTag} = require('../../../utils/common');
 
 require('./index.less');
 
@@ -15,12 +15,21 @@ module.exports = Vue.extend({
 
 	data: () => ({
 		loading: false,
-		suggestions: []
+		suggestions: [],
+		taglist: [],
+		showingAll: false,
 	}),
 
 	computed: {
 		tagColors() {
 			return this.getStory().tagColors;
+		},
+		taglist() {
+			if (this.$data.showingAll) {
+				return this.taglist = this.passage.tags;
+			} else {
+				return this.taglist = this.passage.tags.slice(0, 14);
+			}
 		}
 	},
 
@@ -35,8 +44,8 @@ module.exports = Vue.extend({
 		}
 	},
 
-  events: {
-    'tag-change'(tag) {
+	events: {
+		'tag-change'(tag) {
 			new TagsDialog({
 				data: {
 					storyId: this.storyId,
@@ -46,7 +55,7 @@ module.exports = Vue.extend({
 				},
 				store: this.$store,
 			}).$mountTo(this.$el);
-    },
+		},
 
 		'tag_suggestion'(tag) {
 			const text = buzzwordFromTag(tag);
@@ -70,10 +79,10 @@ module.exports = Vue.extend({
 			const storageData = localStorage.getItem('openai-params');
 			if (storageData) {
 				try {
-					const placeholders = { "%TAG%": text };
-					data = { ...data, ...JSON.parse(storageData) };
+					const placeholders = {"%TAG%": text};
+					data = {...data, ...JSON.parse(storageData)};
 					data.prompt = data.prompt.replace(/%\w+%/g, (placeholder) => placeholders[placeholder] || placeholder);
-				} catch(e) {
+				} catch (e) {
 					notify(e.message, 'danger');
 				}
 			}
@@ -97,7 +106,7 @@ module.exports = Vue.extend({
 				this.suggestions = uniq(suggestions.filter(suggestion => suggestion.length < 30 && !tags.includes(suggestion)));
 				if (!this.suggestions.length) {
 					if (response.choices) {
-						const text = response.choices.map(it => it.text).reduce((acc, it) => acc + it)
+						const text = response.choices.map(it => it.text).reduce((acc, it) => acc + it);
 						notify('No suggestions were found. Response: ' + text, 'info');
 					} else {
 						notify('No suggestions were found.', 'info');
@@ -107,7 +116,7 @@ module.exports = Vue.extend({
 			.catch((error) => notify(error.message, 'danger'))
 			.finally(() => this.loading = false);
 		}
-  },
+	},
 
 	template: require('./index.html'),
 
@@ -140,14 +149,18 @@ module.exports = Vue.extend({
 				}
 			);
 			this.suggestions.splice(this.suggestions.findIndex(s => s === suggestion), 1);
-		}
+		},
+
+		toggleShowTags() {
+			this.$data.showingAll = !this.$data.showingAll;
+		},
 	},
 
 	vuex: {
 		getters: {
 			allStories: state => state.story.stories
 		},
-		actions: { setTagColorInStory, updatePassage }
+		actions: {setTagColorInStory, updatePassage}
 	},
 
 	components: {
