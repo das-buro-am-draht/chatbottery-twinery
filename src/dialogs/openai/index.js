2ec73922a439c6b814d5bd1fb4c1d5178f2bf751
Thorsten Breug <thorsten.breug@ext.conrad.com>
2023-07-05 11:09:58 +0200

    extra component for openai dialog

--- src/dialogs/openai/index.js
+++ src/dialogs/openai/index.js
@@ -12,90 +12,36 @@ module.exports = Vue.extend({
 
 	data: () => ({
 		origin: null,
-		openaiTags: {
-			data: '',
-			message: '',
-		},
-		openaiPhrases: {
-			data: '',
-			message: '',
-		},
+		openaiTags: '',
+		openaiPhrases: '',
 	}),
 
   ready() {
-		this.openaiTags.data = this.getPref.openaiTags;
-		this.openaiPhrases.data = this.getPref.openaiPhrases;
+		this.openaiTags = this.getPref.openaiTags;
+		this.openaiPhrases = this.getPref.openaiPhrases;
   },
 
 	computed: {
 
-		placeholderTag() {
-			return placeholders.tag;
+		placeholders() {
+			return placeholders;
 		},
 
-		placeholderPhrase() {
-			return placeholders.phrase;
+		openaiDefault() {
+			return openaiDefault;
 		},
 
-		isValidOpenaiTags() {
-			try {
-				JSON.parse(this.openaiTags.data);
-				this.openaiTags.message = '';
-				return !!this.openaiTags.data.trim();
-			} catch(e) {
-				this.openaiTags.message = e.message;
-				// if (e.message) {
-				// 	const searchString = 'at position ';
-				// 	const p = e.message.indexOf(searchString);
-				// 	if (p >= 0) {
-				// 		const matches = e.message.substring(p + searchString.length).match(/\d+/);
-				// 		const position = parseInt(matches[0]);
-				// 		if (!isNaN(position)) {
-				// 			this.$els.openaiTags.setSelectionRange(position, position + 1);
-				// 		}
-				// 	}
-				// }
-				return false;
-			}
-		},
-
-		isOpenaiTagPresent() {
-			return this.openaiTags.data.indexOf(placeholders.tag) >= 0;
-		},
-
-		isValidOpenaiPhrases() {
-			try {
-				JSON.parse(this.openaiPhrases.data);
-				this.openaiPhrases.message = '';
-				return !!this.openaiPhrases.data.trim();
-			} catch(e) {
-				this.openaiPhrases.message = e.message;
-				return false;
-			}
-		},
-
-		isOpenaiAltPresent() {
-			return this.openaiPhrases.data.indexOf(placeholders.phrase) >= 0;
-		},
-
-		isValid() {
-			return this.isValidOpenaiTags && this.isValidOpenaiPhrases;
+		canSave() {
+			return this.$refs.tag.isValid 
+					&& this.$refs.phrase.isValid;
 		},
 	},
 
 	methods: {
 
-		resetOpenaiTags() {
-			this.openaiTags.data = openaiDefault.tags;
-		},
-
-		resetOpenaiPhrases() {
-			this.openaiPhrases.data = openaiDefault.phrases;
-		},
-
 		save() {
-			this.setPref('openaiTags', this.openaiTags.data);
-			this.setPref('openaiPhrases', this.openaiPhrases.data);
+			this.setPref('openaiTags', this.$refs.tag.params);
+			this.setPref('openaiPhrases', this.$refs.phrase.params);
 			this.$refs.modal.close();
 		},
 	},
@@ -110,6 +56,7 @@ module.exports = Vue.extend({
 	},
 
 	components: {
-		'modal-dialog': require('../../ui/modal-dialog')
+		'modal-dialog': require('../../ui/modal-dialog'),
+		'openai-params': require('./params'),
 	}
 });
