e17f1aba21d2debfdfb972da3743292b5ad1335d
Thorsten Breug <thorsten.breug@ext.conrad.com>
2023-07-13 16:31:50 +0200

    styling / openai

--- src/data/store/openai.js
+++ src/data/store/openai.js
@@ -1,6 +1,6 @@
 const { placeholders } = require('../../common/app/openai');
 
 module.exports = {
-  tags: `{\n    "model": "gpt-3.5-turbo",\n    "messages": [{ \n      "role": "assistant",\n      "content": "Erzeuge fünf Schlagworte zum Begriff \'${placeholders.tag}\' getrennt durch das Zeilentrennzeichen ('\\n')"\n    }]\n}`,
-  phrases: `{\n    "model": "gpt-3.5-turbo",\n    "messages": [{ \n      "role": "assistant",\n      "content": "Formuliere drei Alternativen zum Text \'${placeholders.phrase}\' getrennt durch das Zeilentrennzeichen ('\\n')"\n    }]\n}`,
+  tags: `{\n    "model": "gpt-3.5-turbo",\n    "messages": [{ \n      "role": "assistant",\n      "content": "Erzeuge fünf Schlagworte zum Begriff \'${placeholders.tag}\'"\n    }]\n}`,
+  phrases: `{\n    "model": "gpt-3.5-turbo",\n    "messages": [{ \n      "role": "assistant",\n      "content": "Formuliere drei Alternativen zum Text \'${placeholders.phrase}\' durch Zeilentrennzeichen (line feed '\\n') getrennt"\n    }]\n}`,
 }
