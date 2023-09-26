090f67fd5484d0466712c2342f88dabdf61217a8
Thorsten Breug <thorsten.breug@ext.conrad.com>
2023-09-26 17:15:05 +0200

    matoma statistics / version 11.4.9

---
+++ src/dialogs/matomo/index.js
@@ -0,0 +1,169 @@
+/*
+A modal which shows aggregrate statistics for a story.
+*/
+
+const Vue = require('vue');
+const locale = require('../../locale');
+const notify = require('../../ui/notify');
+const { stringFromDate, queryParams } = require('../../utils/common');
+const { proxy } = require('../../utils/proxy');
+const specialPassages = require('../../data/special-passages');
+
+require('./index.less');
+
+module.exports = Vue.extend({
+	template: require('./index.html'),
+
+	data: () => ({
+		storyId: '',
+		origin: null,
+		date: {
+			from: stringFromDate(new Date(Date.now() - 864000000)), // 10 days
+			to: stringFromDate(new Date()),
+		},
+		visits: null,
+		items: [[],[],[]],
+		tab: 0,
+	}),
+
+	ready() {
+		this.loadMatomo();
+	},
+
+	computed: {
+		story() {
+			return this.allStories.find(story => story.id === this.storyId);
+		},
+
+		label() {
+			switch (this.tab) {
+				case 0: return 'This list shows the user phrases that couldn\'t be resolved.';
+				case 1: return 'This list shows the user phrases that let to different passage suggestions.';
+				case 2: return 'This list shows the names of passages and the count of invocations.';
+			}
+		},
+
+		totalVisits() {
+			return this.visits.nb_visits;
+		},
+
+		timePerVisit() {
+			if (this.visits.avg_time_on_site) {
+				return this.visits.avg_time_on_site;
+			} else if (this.visits.sum_visit_length && this.visits.nb_visits > 0) {
+				return Math.floor(this.visits.sum_visit_length / this.visits.nb_visits);
+			} else {
+				return '';
+			}
+		},
+
+		actionsPerVisit() {
+			if (this.visits.nb_actions_per_visit !== undefined) {
+				return this.visits.nb_actions_per_visit;
+			} else if (this.visits.nb_actions && this.visits.nb_visits > 0) {
+				return Math.round(((this.visits.nb_actions / this.visits.nb_visits) * 100) / 100);
+			} else {
+				return '';
+			}
+		},
+
+		passagesVisited() {
+			const passages = this.story.passages.length;
+			if (!passages) {
+				return '';
+			} else {
+				return Math.floor((this.items[2].reduce((prev, item) => {
+					if (item.events > 0) {
+						prev += 1;
+					}
+					return prev;
+				}, 0) / passages) * 100) + '%';
+			}
+		},
+	},
+
+	methods: {
+		loadMatomo() {
+			const story = this.allStories.find(story => story.id === this.storyId);
+			const matomo = story.plugins.matomo;
+			if (matomo && matomo.url && matomo.authToken) {
+				const url = matomo.url.replace(/(\S+\/)(\S+\.php\/?)?$/i, '$1');
+				if (url) {
+					const date = new Date(this.date.from).toISOString().slice(0, 10) + ',' +
+					             new Date(this.date.to  ).toISOString().slice(0, 10);
+					const urls = [
+						{
+							method: 'VisitsSummary.get',
+							idSite: matomo.siteId,
+							period: 'range',
+							date,
+						},
+						{
+							method: 'Events.getAction',
+							idSite: matomo.siteId,
+							period: 'range',
+							date,
+							expanded: 1,
+						},
+					]
+					const params = {
+						token_auth: matomo.authToken,
+						module: 'API',
+						method: 'API.getBulkRequest',
+						format: 'JSON',
+						'urls[0]': queryParams(urls[0]),
+						'urls[1]': queryParams(urls[1]),
+					};
+					const queryString = queryParams(params);
+					const requestUrl = proxy.fetch() + '&url=' + encodeURIComponent(`${url}?${queryString}`);
+					fetch(requestUrl)
+						.then((response) => {
+							if (!response.ok) {
+								throw new Error(`HTTP-Status: ${response.status}`);
+							}
+							return response.json();
+						})
+						.then(([visits, events]) => {
+							const items = [[],[],[]];
+							items[2] = this.story.passages.map((passage) => ({label: passage.name, events: 0}));
+							events.forEach((item) => {
+								switch (item.label) {
+									case specialPassages.unmatched:
+										items[0] = (item.subtable || []).map((entry) => ({label: entry.label, events: entry.nb_events}));
+										break;
+									case specialPassages.choosePassage:
+										items[1] = (item.subtable || []).map((entry) => ({label: entry.label, events: entry.nb_events}));
+										break;
+									default:
+										const entry = items[2].find((passage) => passage.label === item.label);
+										if (entry) {
+											entry.events += item.nb_events;
+										} else {
+											items[2].push({label: item.label, events: item.nb_events});
+										}										
+										break;
+								}
+							})
+							this.visits = visits;
+							this.items = items.map((list) => list = list.sort((a, b) => 
+							b.events !== a.events ? b.events - a.events : String(a.label).localeCompare(b.label)
+							));
+						})
+						.catch((error) => {
+							notify(locale.say(`Error on loading Matomo data from '${url}: ${error.message}`), 'danger');
+						});
+				}
+			}
+		},
+	},
+
+	vuex: {
+		getters: {
+			allStories: state => state.story.stories
+		}
+	},
+
+	components: {
+		'modal-dialog': require('../../ui/modal-dialog')
+	}
+});
--- src/dialogs/story-stats/index.js
+++ src/dialogs/story-stats/index.js
@@ -6,13 +6,8 @@ const Vue = require('vue');
 const moment = require('moment');
 const linkParser = require('../../data/link-parser');
 const locale = require('../../locale');
-const notify = require('../../ui/notify');
-const { stringFromDate } = require('../../utils/common');
-const { proxy } = require('../../utils/proxy');
 
 require('./index.less');
-
-const UNMATCHED_PASSAGE = "UNMATCHED";
 
 module.exports = Vue.extend({
 	template: require('./index.html'),
@@ -20,61 +15,7 @@ module.exports = Vue.extend({
 	data: () => ({
 		storyId: '',
 		origin: null,
-		matomo: {
-			from: stringFromDate(new Date(Date.now() - 864000000)), // 10 days
-			to: stringFromDate(new Date()),
-			items: null,
-		},
 	}),
-
-	ready() {
-		this.loadMatomo();
-	},
-
-	methods: {
-		loadMatomo() {
-			const story = this.allStories.find(story => story.id === this.storyId);
-			const matomo = story.plugins.matomo;
-			if (matomo && matomo.url && matomo.authToken) {
-				const url = matomo.url.replace(/(\S+\/)(\S+\.php\/?)?$/i, '$1');
-				if (url) {
-					const params = {
-						token_auth: matomo.authToken,
-						module: 'API',
-						method: 'Events.getAction',
-						idSite: matomo.siteId,
-						period: 'range',
-						date: new Date(this.matomo.from).toISOString().slice(0, 10) + ','
-								+ new Date(this.matomo.to).toISOString().slice(0, 10),
-						format: 'JSON',
-						segment: 'eventAction==' + UNMATCHED_PASSAGE,
-						// filter_limit: 1,
-						expanded: 1,
-					};
-					const queryString = Object.keys(params).map((key) => 
-						encodeURIComponent(key) + '=' + encodeURIComponent(params[key])
-					).join('&');
-					const requestUrl = proxy.fetch() + '&url=' + encodeURIComponent(`${url}?${queryString}`);
-					fetch(requestUrl)
-						.then((response) => {
-							if (!response.ok) {
-								throw new Error(`HTTP-Status: ${response.status}`);
-							}
-							return response.json();
-						})
-						.then((data) => {
-							const unmatched = data.find((item) => item.label === UNMATCHED_PASSAGE);
-							this.matomo.items = unmatched ? (unmatched.subtable || [])
-									.map((entry) => ({label: entry.label, events: entry.nb_events}))
-									.sort((a, b) => b.events - a.events) : [];
-						})
-						.catch((error) => {
-							notify(locale.say(`Error on loading Matomo data from '${url}: ${error.message}`), 'danger');
-						});
-				}
-			}
-		},
-	},
 
 	computed: {
 		story() {
